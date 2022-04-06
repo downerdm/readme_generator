ecb9ce6c133e31178383e5cdeb43edb39b15cf29
Dennis Downer <downerdm@gmail.com>
2022-04-06 15:38:22 -0400

    Initial upload

---
+++ Develop/README.md
@@ -0,0 +1,50 @@
+
+  # Project
+
+
+  ## Description
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur tortor a mi ultrices sodales. Maecenas consequat fringilla sagittis.
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [Questions?](#questions)
+
+## Installation
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur tortor a mi ultrices sodales. Maecenas consequat fringilla sagittis.
+
+
+## Usage
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur tortor a mi ultrices sodales. Maecenas consequat fringilla sagittis.
+
+
+## License
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Contribution
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur tortor a mi ultrices sodales. Maecenas consequat fringilla sagittis.
+
+
+## Tests
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur tortor a mi ultrices sodales. Maecenas consequat fringilla sagittis.
+
+
+## Questions
+
+Here is how to contact me if you have any additional questions:
+
+<https://github.com/downerdm>
+
+<downerdm@gmail.com>
+
---
+++ Develop/index.js
@@ -0,0 +1,69 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Describe any installation instructions',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What will the app be used for?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'What are the contribution guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions?',
+  },
+  {
+    type: 'list',
+    message: 'What is your license?',
+    name: 'license',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD-2'],
+  },
+  {
+    type: 'input',
+    name: 'gitHubUser',
+    message: 'What is your gitHub user name?',
+  },{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
+
+function writeToFile(data) {
+    const markdownData = generateMarkdown(data)
+    const filename = "README.md";
+    fs.writeFile(filename, markdownData, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  };
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {writeToFile(data)});
+}
+
+// Function call to initialize app
+init();
